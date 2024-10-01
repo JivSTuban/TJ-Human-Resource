63428d1a2d4811a2fbda6bc8ad0a9f1d6972f43f
jiv <jeremiatuban@Jivs-iMac.local>
2024-10-01 22:13:25 +0800

    progress

--- TJ_api/TJ/api/models.py
+++ TJ_api/TJ/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django_countries.fields import CountryField  
 # Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20)
+    country = CountryField()
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+class Job(models.Model):
+    job_title = models.CharField(max_length=50)
+    job_description = models.TextField()
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+class User(models.Model):
+    class RoleType(models.TextChoices):
+        EMPLOYEE = 'employee', 'Employee'
+        MANAGER = 'Manager', 'manager'
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    role = models.CharField(choices=RoleType.choices, default=RoleType.EMPLOYEE)
+    date_of_hire = models.DateField()
+    job_id = models.OneToOneField(Job, on_delete=models.CASCADE)
--- TJ_api/TJ/api/serializers.py
+++ TJ_api/TJ/api/serializers.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import User
+from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'first_name', 'last_name', 'address', 'phone_number', 'email', 'role', 'date_of_hire']
